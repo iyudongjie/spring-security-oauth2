0804c24a9d8274d72d27b959b2c2e44cdf4b9da3
yudongjie <iyudongjie@163.com>
2019-12-25 15:40:52 +0800

    commit oauth2.0

--- spring-security-oauth2-server/src/main/java/com/funtl/oauth2/OAuth2ServerApplication.java
+++ spring-security-oauth2-server/src/main/java/com/funtl/oauth2/OAuth2ServerApplication.java
@@ -2,12 +2,15 @@ package com.funtl.oauth2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * @Author: yudongjie
  * @CreateDate: 2019-12-24 21:54
  */
 @SpringBootApplication
+@MapperScan(basePackages = "com.funtl.oauth2.server.mapper")
+
 public class OAuth2ServerApplication {
 
     public static void main(String[] args) {
